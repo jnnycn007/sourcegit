jnnycn007/sourcegit

commit c3cbc6163706574e776619814c8dc9897801dd06

refactor: keep selection in `HISTORY` page if user only selected one or two commits (#2297)
Signed-off-by: leo <longshuang@msn.cn>

src/ViewModels/Histories.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -62,12 +61,8 @@ namespace SourceGit.ViewModels
             get => _commits;
             set
             {
-                var lastSelected = SelectedCommit;
                 if (SetProperty(ref _commits, value))
-                {
-                    if (value.Count > 0 && lastSelected != null)
-                        SelectedCommit = value.Find(x => x.SHA.Equals(lastSelected.SHA, StringComparison.Ordinal));
-                }
+                    PostCommitsChanged();
             }
         }
 
@@ -77,16 +72,14 @@ namespace SourceGit.ViewModels
             set => SetProperty(ref _graph, value);
         }
 
-        public Models.Commit SelectedCommit
+        public List<Models.Commit> SelectedCommits
         {
-            get => _selectedCommit;
-            set => SetProperty(ref _selectedCommit, value);
-        }
-
-        public long NavigationId
-        {
-            get => _navigationId;
-            private set => SetProperty(ref _navigationId, value);
+            get => _selectedCommits;
+            set
+            {
+                if (SetProperty(ref _selectedCommits, value))
+                    PostSelectedCommitsChanged();
+            }
         }
 
         public object DetailContext
@@ -167,8 +160,7 @@ namespace SourceGit.ViewModels
             var commit = _commits.Find(x => x.SHA.StartsWith(commitSHA, StringComparison.Ordinal));
             if (commit != null)
             {
-                SelectedCommit = commit;
-                NavigationId = _navigationId + 1;
+                SelectedCommits = [commit];
                 return;
             }
 
@@ -181,7 +173,7 @@ namespace SourceGit.ViewModels
                 Dispatcher.UIThread.Post(() =>
                 {
                     _ignoreSelectionChange = true;
-                    SelectedCommit = null;
+                    SelectedCommits = [];
 
                     if (_detailContext is CommitDetail detail)
                     {
@@ -199,49 +191,30 @@ namespace SourceGit.ViewModels
             });
         }
 
-        public void Select(IList commits)
+        public void SetSelectedCommitsDirectly(List<Models.Commit> commits)
         {
-            if (_ignoreSelectionChange)
+            if (_selectedCommits.Count != commits.Count)
+            {
+                SelectedCommits = commits;
                 return;
-
-            if (commits.Count == 0)
-            {
-                _repo.SearchCommitContext.Selected = null;
-                DetailContext = null;
             }
-            else if (commits.Count == 1)
+
+            var hashes = new HashSet<string>();
+            foreach (var c in _selectedCommits)
+                hashes.Add(c.SHA);
+
+            var equals = true;
+            foreach (var c in commits)
             {
-                var commit = (commits[0] as Models.Commit)!;
-                if (_repo.SearchCommitContext.Selected == null || !_repo.SearchCommitContext.Selected.SHA.Equals(commit.SHA, StringComparison.Ordinal))
-                    _repo.SearchCommitContext.Selected = _repo.SearchCommitContext.Results?.Find(x => x.SHA.Equals(commit.SHA, StringComparison.Ordinal));
-
-                SelectedCommit = commit;
-                NavigationId = _navigationId + 1;
-
-                if (_detailContext is CommitDetail detail)
+                if (!hashes.Contains(c.SHA))
                 {
-                    detail.Commit = commit;
-                }
-                else
-                {
-                    var commitDetail = new CommitDetail(_repo, _commitDetailSharedData);
-                    commitDetail.Commit = commit;
-                    DetailContext = commitDetail;
+                    equals = false;
+                    break;
                 }
             }
-            else if (commits.Count == 2)
-            {
-                _repo.SearchCommitContext.Selected = null;
 
-                var end = commits[0] as Models.Commit;
-                var start = commits[1] as Models.Commit;
-                DetailContext = new RevisionCompare(_repo, start, end);
-            }
-            else
-            {
-                _repo.SearchCommitContext.Selected = null;
-                DetailContext = new Models.Count(commits.Count);
-            }
+            if (!equals)
+                SelectedCommits = commits;
         }
 
         public async Task<Models.Commit> GetCommitAsync(string sha)
@@ -436,14 +409,79 @@ namespace SourceGit.ViewModels
             DetailContext = new RevisionCompare(_repo, commit, null);
         }
 
+        private void PostCommitsChanged()
+        {
+            if (_commits.Count == 0 ||
+                _selectedCommits.Count == 0 ||
+                _selectedCommits.Count > 2)
+            {
+                SelectedCommits = [];
+                return;
+            }
+
+            var set = new HashSet<string>();
+            foreach (var c in _selectedCommits)
+                set.Add(c.SHA);
+
+            var selected = new List<Models.Commit>();
+            foreach (var c in _commits)
+            {
+                if (set.Contains(c.SHA))
+                {
+                    selected.Add(c);
+                    set.Remove(c.SHA);
+                    if (set.Count == 0)
+                        break;
+                }
+            }
+
+            SelectedCommits = selected;
+        }
+
+        private void PostSelectedCommitsChanged()
+        {
+            if (_ignoreSelectionChange)
+                return;
+
+            if (_selectedCommits.Count == 0)
+            {
+                _repo.SearchCommitContext.Selected = null;
+                DetailContext = null;
+            }
+            else if (_selectedCommits.Count == 1)
+            {
+                var c = _selectedCommits[0];
+                if (_repo.SearchCommitContext.Selected == null || !_repo.SearchCommitContext.Selected.SHA.Equals(c.SHA, StringComparison.Ordinal))
+                    _repo.SearchCommitContext.Selected = _repo.SearchCommitContext.Results?.Find(x => x.SHA.Equals(c.SHA, StringComparison.Ordinal));
+
+                if (_detailContext is CommitDetail detail)
+                    detail.Commit = c;
+                else
+                    DetailContext = new CommitDetail(_repo, _commitDetailSharedData) { Commit = c };
+            }
+            else if (_selectedCommits.Count == 2)
+            {
+                _repo.SearchCommitContext.Selected = null;
+
+                if (_detailContext is RevisionCompare compare)
+                    compare.SetTargets(_selectedCommits[0], _selectedCommits[1]);
+                else
+                    DetailContext = new RevisionCompare(_repo, _selectedCommits[0], _selectedCommits[1]);
+            }
+            else
+            {
+                _repo.SearchCommitContext.Selected = null;
+                DetailContext = new Models.Count(_selectedCommits.Count);
+            }
+        }
+
         private Repository _repo = null;
         private CommitDetailSharedData _commitDetailSharedData = null;
         private bool _isLoading = true;
         private List<Models.Commit> _commits = new List<Models.Commit>();
         private Models.CommitGraph _graph = null;
-        private Models.Commit _selectedCommit = null;
+        private List<Models.Commit> _selectedCommits = [];
         private Models.Bisect _bisect = null;
-        private long _navigationId = 0;
         private object _detailContext = null;
         private bool _ignoreSelectionChange = false;
 

src/ViewModels/Fetch.cs
@@ -68,7 +68,10 @@ namespace SourceGit.ViewModels
         {
             using var lockWatcher = _repo.LockWatcher();
 
-            var navigateToUpstreamHEAD = _repo.SelectedView is Histories { SelectedCommit: { IsCurrentHead: true } };
+            var navigateToUpstreamHEAD = _repo.SelectedView is Histories histories &&
+                histories.SelectedCommits.Count == 1 &&
+                histories.SelectedCommits[0].IsCurrentHead;
+
             var notags = _repo.UIStates.FetchWithoutTags;
             var force = _repo.UIStates.EnableForceOnFetch;
             var log = _repo.CreateLog("Fetch");

src/ViewModels/RevisionCompare.cs
@@ -109,6 +109,23 @@ namespace SourceGit.ViewModels
             Refresh();
         }
 
+        public void SetTargets(Models.Commit l, Models.Commit r)
+        {
+            var hashes = new HashSet<string>();
+            hashes.Add(l.SHA);
+            hashes.Add(r.SHA);
+
+            if (_startPoint is Models.Commit s &&
+                _endPoint is Models.Commit e &&
+                hashes.Contains(s.SHA) &&
+                hashes.Contains(e.SHA))
+                return;
+
+            _startPoint = l;
+            _endPoint = r;
+            Refresh();
+        }
+
         public void OpenChangeWithExternalDiffTool(Models.Change change)
         {
             var opt = new Models.DiffOption(GetSHA(_startPoint), GetSHA(_endPoint), change);

src/Views/Histories.axaml
@@ -40,7 +40,6 @@
                 Background="{DynamicResource Brush.DataGridHeaderBG}"
                 SelectionMode="Extended"
                 ItemsSource="{Binding Commits, Mode=OneWay}"
-                SelectedItem="{Binding SelectedCommit, Mode=OneWay}"
                 CanUserReorderColumns="False"
                 CanUserResizeColumns="True"
                 CanUserSortColumns="False"

src/Views/Histories.axaml.cs
@@ -115,13 +115,13 @@ namespace SourceGit.Views
             set => SetValue(OnlyHighlightCurrentBranchProperty, value);
         }
 
-        public static readonly StyledProperty<long> NavigationIdProperty =
-            AvaloniaProperty.Register<Histories, long>(nameof(NavigationId));
+        public static readonly StyledProperty<List<Models.Commit>> SelectedCommitsProperty =
+            AvaloniaProperty.Register<Histories, List<Models.Commit>>(nameof(SelectedCommits), []);
 
-        public long NavigationId
+        public List<Models.Commit> SelectedCommits
         {
-            get => GetValue(NavigationIdProperty);
-            set => SetValue(NavigationIdProperty, value);
+            get => GetValue(SelectedCommitsProperty);
+            set => SetValue(SelectedCommitsProperty, value);
         }
 
         public static readonly StyledProperty<bool> IsScrollToTopVisibleProperty =
@@ -142,10 +142,21 @@ namespace SourceGit.Views
         {
             base.OnPropertyChanged(change);
 
-            if (change.Property == NavigationIdProperty)
+            if (change.Property == SelectedCommitsProperty &&
+                CommitListContainer.IsLoaded &&
+                !_ignoreSelectionChanged)
             {
-                if (CommitListContainer is { SelectedItems.Count: 1, IsLoaded: true } dataGrid)
-                    dataGrid.ScrollIntoView(dataGrid.SelectedItem, null);
+                _ignoreSelectionChanged = true;
+
+                var dataGrid = CommitListContainer;
+                dataGrid.SelectedItems.Clear();
+                if (SelectedCommits is { Count: > 0 })
+                {
+                    foreach (var c in SelectedCommits)
+                        dataGrid.SelectedItems.Add(c);
+                }
+
+                _ignoreSelectionChanged = false;
             }
         }
 
@@ -156,8 +167,16 @@ namespace SourceGit.Views
             if (rowsPresenter is { Children: { Count: > 0 } rows })
                 CommitGraph.Layout = new(0, dataGrid.Columns[0].ActualWidth - 4, rows[0].Bounds.Height);
 
-            if (dataGrid.SelectedItems.Count == 1)
-                dataGrid.ScrollIntoView(dataGrid.SelectedItem, null);
+            _ignoreSelectionChanged = true;
+
+            dataGrid.SelectedItems.Clear();
+            if (SelectedCommits is { Count: > 0 })
+            {
+                foreach (var c in SelectedCommits)
+                    dataGrid.SelectedItems.Add(c);
+            }
+
+            _ignoreSelectionChanged = false;
         }
 
         private async void OnGotoParent(object sender, RoutedEventArgs e)
@@ -299,10 +318,28 @@ namespace SourceGit.Views
                 CommitListContainer.ScrollIntoView(histories.Commits[0], null);
         }
 
-        private void OnCommitListSelectionChanged(object _, SelectionChangedEventArgs e)
+        private void OnCommitListSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (DataContext is ViewModels.Histories histories)
-                histories.Select(CommitListContainer.SelectedItems);
+            if (sender is not DataGrid dataGrid)
+                return;
+
+            var selected = dataGrid.SelectedItems;
+            var commits = new List<Models.Commit>();
+            foreach (var o in selected)
+            {
+                if (o is Models.Commit c)
+                    commits.Add(c);
+            }
+
+            if (commits.Count > 0 && commits.Count < 3)
+                dataGrid.ScrollIntoView(commits[^1], null);
+
+            if (DataContext is ViewModels.Histories histories && !_ignoreSelectionChanged)
+            {
+                _ignoreSelectionChanged = true;
+                histories.SetSelectedCommitsDirectly(commits);
+                _ignoreSelectionChanged = false;
+            }
 
             e.Handled = true;
         }
@@ -1437,5 +1474,6 @@ namespace SourceGit.Views
         private double _lastGraphStartY = 0;
         private double _lastGraphClipWidth = 0;
         private double _lastGraphRowHeight = 0;
+        private bool _ignoreSelectionChanged = false;
     }
 }

src/Views/Repository.axaml
@@ -927,10 +927,10 @@
           
           <DataTemplate DataType="vm:Histories">
             <v:Histories CurrentBranch="{Binding $parent[v:Repository].((vm:Repository)DataContext).CurrentBranch}"
-                         Bisect="{Binding Bisect}"
+                         Bisect="{Binding Bisect, Mode=OneWay}"
+                         SelectedCommits="{Binding SelectedCommits, Mode=OneWay}"
                          IssueTrackers="{Binding $parent[v:Repository].((vm:Repository)DataContext).IssueTrackers}"
-                         OnlyHighlightCurrentBranch="{Binding $parent[v:Repository].((vm:Repository)DataContext).OnlyHighlightCurrentBranchInHistory}"
-                         NavigationId="{Binding NavigationId}"/>
+                         OnlyHighlightCurrentBranch="{Binding $parent[v:Repository].((vm:Repository)DataContext).OnlyHighlightCurrentBranchInHistory}"/>
           </DataTemplate>
 
           <DataTemplate DataType="vm:WorkingCopy">

commit baf0dd00549c171de9f8409e1a2652348cc8cf9e

fix: wrong commit order to compare which is introduced by commit c3cbc61637
Signed-off-by: leo <longshuang@msn.cn>

src/ViewModels/Histories.cs
@@ -468,9 +468,9 @@ namespace SourceGit.ViewModels
                 _repo.SearchCommitContext.Selected = null;
 
                 if (_detailContext is RevisionCompare compare)
-                    compare.SetTargets(_selectedCommits[0], _selectedCommits[1]);
+                    compare.SetTargets(_selectedCommits[1], _selectedCommits[0]);
                 else
-                    DetailContext = new RevisionCompare(_repo, _selectedCommits[0], _selectedCommits[1]);
+                    DetailContext = new RevisionCompare(_repo, _selectedCommits[1], _selectedCommits[0]);
             }
             else
             {